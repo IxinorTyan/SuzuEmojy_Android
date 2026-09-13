bdebdf84ce91355d5808284a93e4d2f576b25b5c
IxinorTyan <1491503128@qq.com>
2026-09-13 20:16:32 +0800

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tencent.qqpinyin.suzu"
         minSdk = 26
         targetSdk = 34
-        versionCode = 100012
-        versionName = "1.5.0"
+        versionCode = 100013
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "FILE_PROVIDER_AUTHORITY", "\"com.tencent.qqpinyin.suzu.provider\"")
